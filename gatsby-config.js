07bfc909fe5cf8ec2262460a4a56e67120b7fc01
Sean Bollinger <bollinger.sean@gmail.com>
2019-08-27 19:32:41 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Sean Bollinger's Blog`,
+    author: `Sean Bollinger`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `Sean13ollinger`,
     },
   },
   plugins: [
